68a0cdb984f9af6aa4d55ca082cec4ed83151f1f
Jip-M <s.j.mijnheer@student.rug.nl>
2025-12-11 13:20:51 +0100

    Outline for fact/rule classes

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
+#
+from typing import Literal
 
-class Printer:
-    def __init__(self, statement:str = "Hello world"):
-        self._statement = statement
-
-    def __call__(self):
-        print(self._statement)
-
-    def __repr__(self):
-        return self._statement
-
-    def update_statement(self, new_statement:str) -> None:
-        self._statement = new_statement
-
-
-myprinter = Printer()
-myprinter()
-print(myprinter)
-
-class Rule:
-    pass
 
 class Fact:
-    pass
+    def __init__(
+        self, label: str, description: str, truth: int = -1, operator=None
+    ) -> None:
+        self._label = label
+        self._description = description
+        self._truth = truth
+        self._operator = operator
+
+    def set_truth(self, new_truth: Literal[0, 1]):
+        self._truth = new_truth
+
+    @property
+    def truth(self):
+        return self._truth
 
 
+class Rule:
+    def __init__(self, ifs: list[Fact], thens: list[Fact]):
+        self._if: list[Fact] = ifs
+        self._then: list[Fact] = thens
+
+    def evaluate(self) -> None:
+        for fact in self._if:
+            if not fact.truth:
+                return
+        for fact in self._then:
+            fact.set_truth(1)
+        return
